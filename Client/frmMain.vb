5d6c404a1bab394d4528a52fd66432992c1cd43d
xmach <xmachforever@hotmail.com>
2014-08-29 17:18:07 +0800

    Update frmMain.vb

--- Client/frmMain.vb
+++ Client/frmMain.vb
@@ -1,7 +1,8 @@
-﻿Public Class frmMain
-    'Public WithEvents mobjSocketClient As TCPConnection
-    Delegate Sub SetTextCallback(ByVal [text] As String)
-    Delegate Sub SetTextCallback2()
+Imports System.Windows.Forms
+Imports LC
+
+Public Class frmMain
+    Private m_frmInstructions As frmInstructions = New frmInstructions()
 
     Private Sub frmMain_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
         Try
@@ -36,30 +37,15 @@
             End If
 
             'connect
-            myIPAddress = getINI(sfile, "Settings", "ip")
-            myPortNumber = getINI(sfile, "Settings", "port")
-            localPort = getINI(sfile, "Settings", "localport")
+            serverIPAddress = getINI(sfile, "Settings", "ip")
+            serverPortNumber = getINI(sfile, "Settings", "port")
+            localPortNumber = getINI(sfile, "Settings", "localport")
             connect()
 
-            BindData()
         Catch ex As Exception
             appEventLog_Write("errorfrmChat_Load :", ex)
         End Try
 
-    End Sub
-    Private Sub BindData()
-        BindRowData(New String() {"10,10", "0,0", "3,3"})
-        BindRowData(New String() {"2,2", "20,20", "0,0"})
-        BindRowData(New String() {"5,5", "3,3", "20,20"})
-    End Sub
-    Private Sub BindRowData(ByVal rowData As String())
-        Dim index As Integer = Me.DataGridView1.Rows.Add()
-        Dim newRow As DataGridViewRow = Me.DataGridView1.Rows(index)
-        For i As Integer = 0 To rowData.Length - 1
-            newRow.Cells(i).Value = rowData(i)
-        Next
-
-        'Me.DataGridView1.Rows.Add(New DataGridViewRow())
     End Sub
 
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
@@ -86,5 +72,54 @@
         Catch ex As Exception
             appEventLog_Write("error :", ex)
         End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        wskClient.Send("02|a|a#")
+    End Sub
+
+    Public Sub DisplayLanguageDecision()
+        'TODO 
+        MessageBox.Show("DisplayLanguageDecision")
+
+    End Sub
+    Public Sub DisplayInstructions()
+        m_frmInstructions.ShowDialog()
+        'send 
+        Dim message As New LC.MessageBag()
+        message.MsgType = MsgType.Client_finishedInstructions
+        modMain.wskClient.Send(LC.XmlHelper.XmlSerialize(message, System.Text.Encoding.UTF8))
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim message As New LC.MessageBag()
+        message.MsgType = MsgType.Client_newRound
+        modMain.wskClient.Send(LC.XmlHelper.XmlSerialize(message, System.Text.Encoding.UTF8))
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim message As New LC.MessageBag()
+        message.MsgType = MsgType.Client_finishedInstructions
+        modMain.wskClient.Send(LC.XmlHelper.XmlSerialize(message, System.Text.Encoding.UTF8))
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim message As New LC.MessageBag()
+        message.MsgType = MsgType.Client_sendPhase1Decision
+        message.Phase1Decision = "a"
+        modMain.wskClient.Send(LC.XmlHelper.XmlSerialize(message, System.Text.Encoding.UTF8))
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim message As New LC.MessageBag()
+        message.MsgType = MsgType.Client_sendLearnInvent
+        message.learn = 1
+        message.invent = New Tuple(Of String, String)() {New Tuple(Of String, String)("a", "*-*")}
+        modMain.wskClient.Send(LC.XmlHelper.XmlSerialize(message, System.Text.Encoding.UTF8))
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+
     End Sub
 End Class
