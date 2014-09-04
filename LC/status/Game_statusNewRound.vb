efb75f15ba0effb199c345148d1bc854a47a97f7
xmach <xmachforever@hotmail.com>
2014-09-04 17:46:33 +0800

    Update Game_statusNewRound.vb
    9.4

--- LC/status/Game_statusNewRound.vb
+++ LC/status/Game_statusNewRound.vb
@@ -6,12 +6,17 @@ Public Class StatusNewRound
     Inherits StatusBase
 
     Public Overrides Sub ReceiveMsg(ByVal m As LC.MessageBag)
-        If m.MsgType = MsgType.Client_sendLearnInvent Then
-            If Game.AllRequiredClientMsgReceived Then
-                Me.Game.UpdateVocabulary()
-                Me.Game.Status = New StatusVocabularyUpdated
-                Me.Game.ShowScoreMatrix()
+        If m.MsgType = LC.MsgType.Client_sendLearnInvent Then
+            If Group.AllRequiredClientMsgReceived(m.MsgType) Then
+                Me.Group.UpdateVocabulary()
+                Me.Group.Status = New StatusVocabularyUpdated(Me.Group)
+                Me.Group.ShowScoreMatrix()
             End If
         End If
+    End Sub
+
+    Public Sub New(ByVal g As Group)
+        MyBase.New(g)
+
     End Sub
 End Class
