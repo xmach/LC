ab6b9eec97cbf4f5ee3b64e0f6e2fb50ea7a0433
xmach <xmachforever@hotmail.com>
2014-09-04 17:28:13 +0800

    Update modMain.vb
    9.4

--- Client/modMain.vb
+++ Client/modMain.vb
@@ -25,14 +25,11 @@ Module modMain
 
     Public closing As Boolean = False
 
-    'Public instructionX As Integer            'start up locations of windows
-    'Public instructionY As Integer
-    'Public windowX As Integer
-    'Public windowY As Integer
-
     'kyle
-    Private m_status As ClientStatusBase = New StatusNotConnected()
     Public m_meanings As String()
+    Public ReadOnly mainForm As New frmMain()
+    Private m_status As ClientStatusBase = New StatusNotConnected()
+
     Public Property GameStatus() As ClientStatusBase
         Get
             Return m_status
@@ -56,7 +53,10 @@ Module modMain
                 If String.IsNullOrEmpty(msgtokens(i - 1)) Then
                     Continue For
                 End If
-                takeMessage(msgtokens(i - 1))
+                'takeMessage(msgtokens(i - 1))
+                Dim msgbag As LC.MessageBag = LC.XmlHelper.XmlDeserialize(Of LC.MessageBag)(msgtokens(i - 1), Encoding.UTF8)
+
+                GameStatus.ReceiveMsg(msgbag)
             Next
 
         Catch ex As Exception
@@ -111,6 +111,8 @@ Module modMain
 
 #Region " General Functions "
     Public Sub main()
+        testDesionScreen()
+        
         ModuleEventLog.fileNamePrefix = "ClientLog"
         AppEventLog_Init()
         appEventLog_Write("Begin")
@@ -118,50 +120,36 @@ Module modMain
         ToggleScreenSaverActive(False)
 
         Application.EnableVisualStyles()
-        Application.Run(frmMain)
+        Application.Run(mainForm)
 
         ToggleScreenSaverActive(True)
 
         appEventLog_Write("End")
         AppEventLog_Close()
     End Sub
+    Private Sub testDesionScreen()
+        Dim f As New frmDecision
+        f.IsPhase2 = False
+        f.ScoreMatrix = New ScoreMatrix()
+        Dim meanings As String() = New String() {"A", "B", "C"}
+        f.ScoreMatrix.ReadFromFile("D:\Project\LC\Server\bin\Debug\server.ini", meanings, "cooperatematrix")
+        f.Meanings = meanings
+        f.ShowDialog()
+    End Sub
+    'test frmLangChange
+    Private Sub testLangeScreen()
+        Dim vo As New Vocabulary()
+        vo.Meanings = New List(Of String)(New String() {"A", "B", "C"})
+        vo.AddCommonList("A", "*")
+        vo.AddPrivateList("B", "()")
+        vo.AddCommonList("C", "$")
+        Dim f As New frmLangChange()
+        f.Vocabulary = vo
+        f.Meaning = vo.Meanings.ToArray()
+        f.Symbols = New String() {"*", "()", "$"}
+        f.ShowDialog()
+    End Sub
 
-    'Public Function getMyColor(ByVal index As Integer) As Color
-    '    Try
-    '        'appEventLog_Write("get color")
-
-    '        Select Case index
-    '            Case 1
-    '                getMyColor = Color.Blue
-    '            Case 2
-    '                getMyColor = Color.Red
-    '            Case 3
-    '                getMyColor = Color.Teal
-    '            Case 4
-    '                getMyColor = Color.Green
-    '            Case 5
-    '                getMyColor = Color.Purple
-    '            Case 6
-    '                getMyColor = Color.Orange
-    '            Case 7
-    '                getMyColor = Color.Brown
-    '            Case 8
-    '                getMyColor = Color.Gray
-    '        End Select
-    '    Catch ex As Exception
-    '        appEventLog_Write("error getMyColor:", ex)
-    '    End Try
-    'End Function
-
-    'Public Function timeConversion(ByVal sec As Integer) As String
-    '    Try
-    '        'appEventLog_Write("time conversion :" & sec)
-    '        timeConversion = Format((sec \ 60), "00") & ":" & Format((sec Mod 60), "00")
-    '    Catch ex As Exception
-    '        appEventLog_Write("error timeConversion:", ex)
-    '        timeConversion = ""
-    '    End Try
-    'End Function
 
     Public Sub setID(ByVal sinstr As String)
         Try
@@ -181,24 +169,24 @@ Module modMain
     End Sub
 
 
-    Public Sub sendIPAddress(ByVal sinstr As String)
-        Try
-            'appEventLog_Write("send ip :" & sinstr)
+    'Public Sub sendIPAddress(ByVal sinstr As String)
+    '    Try
+    '        'appEventLog_Write("send ip :" & sinstr)
 
-            With frmMain
-                'Dim outstr As String
+    '        With frmMain
+    '            'Dim outstr As String
 
-                inumber = sinstr
+    '            inumber = sinstr
 
-                appEventLog_Write("Client# = " & inumber)
+    '            appEventLog_Write("Client# = " & inumber)
 
-                'outstr = SystemInformation.ComputerName
-                '.wskClient.Send("03", outstr)
-            End With
-        Catch ex As Exception
-            appEventLog_Write("error sendIPAddress:", ex)
-        End Try
-    End Sub
+    '            'outstr = SystemInformation.ComputerName
+    '            '.wskClient.Send("03", outstr)
+    '        End With
+    '    Catch ex As Exception
+    '        appEventLog_Write("error sendIPAddress:", ex)
+    '    End Try
+    'End Sub
 
     'Public Function numberSuffix(ByVal sinstr As Integer) As String
     '    numberSuffix = sinstr
@@ -219,97 +207,97 @@ Module modMain
     'End Function
 #End Region
 
-    Private Sub takeMessage(ByVal sinstr As String)
-        Try
-            'take message from server
-            'msgtokens(0) has type of message sent, having different types of messages allows you to send different formats for different actions.
-            'msgtokens(1) has the semicolon delimited data that is to be parsed and acted upon.  
+    'Private Sub takeMessage(ByVal sinstr As String)
+    '    Try
+    '        'take message from server
+    '        'msgtokens(0) has type of message sent, having different types of messages allows you to send different formats for different actions.
+    '        'msgtokens(1) has the semicolon delimited data that is to be parsed and acted upon.  
 
-            'Dim msgtokens() As String
-            'msgtokens = sinstr.Split("|")
-            Dim mTypeStr As String = sinstr 'msgtokens(0)
-            Dim msgbag As LC.MessageBag = LC.XmlHelper.XmlDeserialize(Of LC.MessageBag)(sinstr, Encoding.UTF8)
-            'Dim mType As LC.MsgType = System.Enum.Parse(GetType(LC.MsgType), mTypeStr)
-            GameStatus.ReceiveMsg(msgbag)
-            'Select Case msgtokens(0) 'case statement to handle each of the different types of messages
+    '        'Dim msgtokens() As String
+    '        'msgtokens = sinstr.Split("|")
+    '        Dim mTypeStr As String = sinstr 'msgtokens(0)
+    '        Dim msgbag As LC.MessageBag = LC.XmlHelper.XmlDeserialize(Of LC.MessageBag)(sinstr, Encoding.UTF8)
+    '        'Dim mType As LC.MsgType = System.Enum.Parse(GetType(LC.MsgType), mTypeStr)
+    '        GameStatus.ReceiveMsg(msgbag)
+    '        'Select Case msgtokens(0) 'case statement to handle each of the different types of messages
 
-            'End Select
-        Catch ex As Exception
-            appEventLog_Write("error takeMessage:", ex)
-        End Try
+    '        'End Select
+    '    Catch ex As Exception
+    '        appEventLog_Write("error takeMessage:", ex)
+    '    End Try
 
-    End Sub
+    'End Sub
 
 
 
-    Public Sub begin(ByVal sinstr As String)
-        With frmMain
-            Try
-                'server has signaled client to start experiment
+    'Public Sub begin(ByVal sinstr As String)
+    '    With frmMain
+    '        Try
+    '            'server has signaled client to start experiment
 
-                'parse incoming data string
-                'Dim msgtokens() As String = sinstr.Split(";")
+    '            'parse incoming data string
+    '            'Dim msgtokens() As String = sinstr.Split(";")
 
-                'If showInstructions Then
-                '    'frmInstructions.Show()
-                '    frmInstructions.Location = New System.Drawing.Point(instructionX, instructionY)
-                'End If
+    '            'If showInstructions Then
+    '            '    'frmInstructions.Show()
+    '            '    frmInstructions.Location = New System.Drawing.Point(instructionX, instructionY)
+    '            'End If
 
-                '.Location = New System.Drawing.Point(windowX, windowY)
-            Catch ex As Exception
-                appEventLog_Write("error begin:", ex)
-            End Try
+    '            '.Location = New System.Drawing.Point(windowX, windowY)
+    '        Catch ex As Exception
+    '            appEventLog_Write("error begin:", ex)
+    '        End Try
 
-        End With
-    End Sub
+    '    End With
+    'End Sub
 
-    Public Sub endGame(ByVal sinstr As String)
-        Try
-            'end the experiment
-            With frmMain
+    'Public Sub endGame(ByVal sinstr As String)
+    '    Try
+    '        'end the experiment
+    '        With frmMain
 
-            End With
-        Catch ex As Exception
-            appEventLog_Write("error endGame:", ex)
-        End Try
-    End Sub
+    '        End With
+    '    Catch ex As Exception
+    '        appEventLog_Write("error endGame:", ex)
+    '    End Try
+    'End Sub
 
-    Public Sub endEarly(ByVal sinstr As String)
-        Try
-            'end experiment early
-            Dim msgtokens() As String
-            msgtokens = sinstr.Split(";")
+    'Public Sub endEarly(ByVal sinstr As String)
+    '    Try
+    '        'end experiment early
+    '        Dim msgtokens() As String
+    '        msgtokens = sinstr.Split(";")
 
-            numberOfPeriods = msgtokens(0)
-        Catch ex As Exception
-            appEventLog_Write("error endEarly:", ex)
-        End Try
-    End Sub
+    '        numberOfPeriods = msgtokens(0)
+    '    Catch ex As Exception
+    '        appEventLog_Write("error endEarly:", ex)
+    '    End Try
+    'End Sub
 
-    Public Sub periodResults(ByVal sinstr As String)
-        Try
-            'show the results at end of period
-            With frmMain
+    'Public Sub periodResults(ByVal sinstr As String)
+    '    Try
+    '        'show the results at end of period
+    '        With frmMain
 
-                Dim msgtokens() As String = Split(sinstr, ";")
-                Dim nextToken As Integer = 0
+    '            Dim msgtokens() As String = Split(sinstr, ";")
+    '            Dim nextToken As Integer = 0
 
-            End With
-        Catch ex As Exception
-            appEventLog_Write("error periodResults:", ex)
-        End Try
-    End Sub
+    '        End With
+    '    Catch ex As Exception
+    '        appEventLog_Write("error periodResults:", ex)
+    '    End Try
+    'End Sub
 
-    Public Sub finishedInstructions()
-        Try
-            With frmMain
-                'close the instructions and start experiment
+    'Public Sub finishedInstructions()
+    '    Try
+    '        With frmMain
+    '            'close the instructions and start experiment
 
-                frmInstructions.Close()
-                showInstructions = False
-            End With
-        Catch ex As Exception
-            appEventLog_Write("error :", ex)
-        End Try
-    End Sub
+    '            frmInstructions.Close()
+    '            showInstructions = False
+    '        End With
+    '    Catch ex As Exception
+    '        appEventLog_Write("error :", ex)
+    '    End Try
+    'End Sub
 End Module
