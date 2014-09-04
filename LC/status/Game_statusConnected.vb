c213183b1501bb12adb3eb10daeeaf03eff528d0
xmach <xmachforever@hotmail.com>
2014-09-04 17:44:19 +0800

    Update Game_statusConnected.vb
    9.4

--- LC/status/Game_statusConnected.vb
+++ LC/status/Game_statusConnected.vb
@@ -1,19 +1,20 @@
 Public Class StatusConnected
     Inherits StatusBase
 
-    'Public Sub New(ByVal g As Game)
-    '    MyBase.New(g)
-    '    Me.m_game = g
-
-    'End Sub
+   
     Public Overrides Sub ReceiveMsg(ByVal m As LC.MessageBag)
-        If m.MsgType = MsgType.Client_finishedInstructions Then
-            'TODO and both msg received
-            If Game.AllRequiredClientMsgReceived Then
-                Me.Game.Status = New StatusNewRound
-                Me.Game.SendVocabulary()
+        If m.MsgType = LC.MsgType.Client_finishedInstructions Then
+            If Group.AllRequiredClientMsgReceived(m.MsgType) Then
+                Me.Group.Status = New StatusNewRound(Me.Group)
+                Me.Group.SendVocabulary()
             End If
         End If
     End Sub
+
+    Public Sub New(ByVal g As Group)
+        MyBase.New(g)
+
+    End Sub
+
 
 End Class
