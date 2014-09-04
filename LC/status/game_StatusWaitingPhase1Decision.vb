adac08bf0d8891dd75b388d94d4cfdf98f3f6343
xmach <xmachforever@hotmail.com>
2014-09-04 17:45:28 +0800

    Update game_StatusWaitingPhase1Decision.vb
    9.4

--- LC/status/game_StatusWaitingPhase1Decision.vb
+++ LC/status/game_StatusWaitingPhase1Decision.vb
@@ -2,11 +2,15 @@ Public Class StatusWaitingPhase1Decision
     Inherits StatusBase
 
 
-    Public Overrides Sub ReceiveMsg(ByVal m As MessageBag)
-        If m.MsgType = MsgType.Client_sendPhase1Decision Then
-            If Me.Game.AllRequiredClientMsgReceived Then
-                Me.Game.Status = New StatusWaitingPhase2Decision
+    Public Overrides Sub ReceiveMsg(ByVal m As LC.MessageBag)
+        If m.MsgType = LC.MsgType.Client_sendPhase1Decision Then
+            If Me.Group.AllRequiredClientMsgReceived(m.MsgType) Then
+                Me.Group.Status = New StatusWaitingPhase2Decision(Me.Group)
             End If
         End If
+    End Sub
+    Public Sub New(ByVal g As Group)
+        MyBase.New(g)
+
     End Sub
 End Class
