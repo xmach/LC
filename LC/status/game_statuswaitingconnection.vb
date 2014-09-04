d25e9f5f9ee5df46efcd8222e7a6e29e588e1b9e
xmach <xmachforever@hotmail.com>
2014-09-04 17:47:49 +0800

    Update game_statuswaitingconnection.vb
    9.4

--- LC/status/game_statuswaitingconnection.vb
+++ LC/status/game_statuswaitingconnection.vb
@@ -2,7 +2,12 @@ Public Class StatusWaitingConnection
     Inherits StatusBase
 
 
-    Public Overrides Sub ReceiveMsg(ByVal m As MessageBag)
+    Public Overrides Sub ReceiveMsg(ByVal m As LC.MessageBag)
+
+    End Sub
+
+    Public Sub New(ByVal g As Group)
+        MyBase.New(g)
 
     End Sub
 End Class
