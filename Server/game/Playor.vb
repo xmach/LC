5bdda1433581003821485db8ddb8a8f3fde60c5e
xmach <xmachforever@hotmail.com>
2014-09-10 17:24:23 +0800

    Update Playor.vb
    9/10

--- Server/game/Playor.vb
+++ Server/game/Playor.vb
@@ -3,21 +3,26 @@ Imports System.IO
 Imports System.Xml.Serialization
 Imports System.Text
 
-Public Class Playor
+Public Class Player
     Private m_id As String
     Private m_score As Integer
-    Private m_countpart As Playor
+    Private m_countpart As Player
     Private m_name As String
     Private m_vaca As Vocabulary
     Private m_socketKey As String
     Private m_myIPAddress As String
     Private m_group As Group
     Private m_cost As Integer
+    Private m_phase1_decision As String
+    Private m_phase2_decision As String
+    Private m_num_of_learn As Nullable(Of Integer)
+    Private m_num_of_invent As Nullable(Of Integer)
+    Private m_InitialLS As Nullable(Of Decimal)
+    Private m_FinalLS As Nullable(Of Decimal)
 
     Public Sub New()
 
     End Sub
-
 
     Public Property ID() As String
         Get
@@ -28,7 +33,6 @@ Public Class Playor
             Me.m_id = value
         End Set
     End Property
-
 
     Public Property Name() As String
         Get
@@ -46,10 +50,9 @@ Public Class Playor
         End Get
         Set(ByVal value As Vocabulary)
             Me.m_vaca = value
+            Me.m_InitialLS = value.LS
         End Set
     End Property
-
-
 
     Public Property socketKey() As String
         Get
@@ -60,16 +63,16 @@ Public Class Playor
         End Set
     End Property
     ''' <summary>
-    ''' the other playor(group)
+    ''' the other Player(group)
     ''' </summary>
     ''' <value></value>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    Public Property Counterpart() As Playor
+    Public Property Counterpart() As Player
         Get
             Return Me.m_countpart
         End Get
-        Set(ByVal value As Playor)
+        Set(ByVal value As Player)
             Me.m_countpart = value
         End Set
     End Property
@@ -92,6 +95,56 @@ Public Class Playor
         End Set
     End Property
 
+    Public Property Phase1_decision() As String
+        Get
+            Return Me.m_phase1_decision
+        End Get
+        Set(ByVal value As String)
+            Me.m_phase1_decision = value
+        End Set
+    End Property
+
+    Public Property Num_of_learn() As Nullable(Of Integer)
+        Get
+            Return Me.m_num_of_learn
+        End Get
+        Set(ByVal value As Nullable(Of Integer))
+            Me.m_num_of_learn = value
+        End Set
+    End Property
+
+    Public Property Num_of_invent() As Nullable(Of Integer)
+        Get
+            Return Me.m_num_of_invent
+        End Get
+        Set(ByVal value As Nullable(Of Integer))
+            Me.m_num_of_invent = value
+        End Set
+    End Property
+
+    Public Property Phase2_decision() As String
+        Get
+            Return Me.m_phase2_decision
+        End Get
+        Set(ByVal value As String)
+            Me.m_phase2_decision = value
+        End Set
+    End Property
+
+    Public ReadOnly Property InitialLS() As Nullable(Of Decimal)
+        Get
+            Return Me.m_InitialLS
+        End Get
+        'Set(ByVal value As Nullable(Of Decimal))
+        '    Me.m_InitialLS = value
+        'End Set
+    End Property
+
+    Public ReadOnly Property CurrentLS() As Nullable(Of Decimal)
+        Get
+            Return Me.Vocabulary.LS
+        End Get
+    End Property
 
     Public Property myIPAddress() As String
         Get
@@ -102,16 +155,6 @@ Public Class Playor
         End Set
     End Property
 
-    'Private m_IPAddress As String
-    'Public Property IPAddress() As String
-    '    Get
-    '        Return m_IPAddress
-    '    End Get
-    '    Set(ByVal value As String)
-    '        m_IPAddress = value
-    '    End Set
-    'End Property
-
     Public Property Group() As Group
         Get
             Return Me.m_group
@@ -120,6 +163,7 @@ Public Class Playor
             Me.m_group = value
         End Set
     End Property
+
     Public Sub requsetIP(ByVal wsk_Col As WinsockCollection)
         Try
             'TODO wsk_Col.Send(MsgType.requestIP.ToString(), socketNumber, Me.ID)
@@ -198,9 +242,4 @@ Public Class Playor
         End Try
     End Sub
 
-    'Public Sub ProcessMsg(ByVal msg As LC.MessageBag)
-    '    'TODO
-    '    'handle the status
-    '    Me.Group.ProcessMsg(msg, Me)
-    'End Sub
 End Class
